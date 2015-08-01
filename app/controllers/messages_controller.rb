48ccb2cae3668a108656697440ae3e426cdb6cb7
arks1969 <arks1969@gmail.com>
2015-08-01 14:01:06 +0000

    Gemfile update for heroku

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -1,4 +1,6 @@
 class MessagesController < ApplicationController
+  before_action :set_message, only: [:edit, :update, :destroy]
+  
   def index
     # Messageを全て取得する。
     @messages = Message.all
@@ -17,7 +19,27 @@ class MessagesController < ApplicationController
       render 'index'
     end
   end
+  
+  def edit
+  end
+  
+  ## 編集処理の追加
+  def update
+    if @message.update(message_params)
+      # 保存に成功した場合はトップページへリダイレクト
+      redirect_to root_path , notice: 'メッセージを編集しました'
+    else
+      # 保存に失敗した場合は編集画面へ戻す
+      render 'edit'
+    end
+  end
 
+  ## 削除処理
+  def destroy
+    @message.destroy
+    redirect_to root_path, notice: 'メッセージを削除しました'
+  end
+  
   # ここから下はprivateメソッドとなる
   private
   def message_params
@@ -26,4 +48,8 @@ class MessagesController < ApplicationController
     params.require(:message).permit(:name, :body)
   end
   ## ここまで
+  
+  def set_message
+    @message = Message.find(params[:id])
+  end
 end
