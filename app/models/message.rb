7813ecc3159cd9729fa34d268a7d895d445adbbb
arks1969 <arks1969@gmail.com>
2015-08-02 14:59:36 +0000

    Codereview challenge2

--- app/models/message.rb
+++ app/models/message.rb
@@ -3,4 +3,7 @@ class Message < ActiveRecord::Base
     validates :name , length: { maximum: 20 } , presence: true
     # 内容は必須入力且つ2文字以上30文字以下
     validates :body , length: { minimum: 2 , maximum: 30 } , presence: true
+    # 年齢は3文字以下
+    #validates :age , numericality: { only_integer: true } ,
+    validates :age , inclusion: { in: 1..120 } , numericality: { only_integer: true }
 end
